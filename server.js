1b9871e2a23b688bc2747729bde15703b84151e8
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-11 12:29:00 -0600

    Established a rudimentary connection to MySQL

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const express = require('express');
 
 const app = express();
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'dev',
+    password : '1234',
+    database: 'test'
+});
 
 const port = process.env.port || 3000;
 
+connection.connect(
+    (err) =>{
+        if (err){
+            console.log("Can't establish connection");
+        }
+
+        else{
+            console.log("Connection established");
+        }
+
+    }
+);
+
 app.get("/", (req, res) => {
+
+    connection.query("select * from words", (error, results, fields) =>{
+        if(error){
+            console.log("Can't process query");
+            return;
+        }
+
+        console.log(results);
+        console.log(results[0].name);
+    });
     res.send("Hello World");
 });
 
 app.listen(port, ()=>{
-    console.log("Server has started!")
+    console.log("Server has started on " + port);
 });
