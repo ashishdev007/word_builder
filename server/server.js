30bec9c6e0d01e61daf50946a8cba9e4c36f662f
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-22 17:33:50 -0600

    Moved the database to remotesql.com and changed code accordingly

--- server/server.js
+++ server/server.js
@@ -2,106 +2,113 @@ const express = require("express");
 
 const app = express();
 const mysql = require("mysql");
+const port = process.env.port || 1500;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "dev",
-  password: "1234",
-  database: "words"
-});
+var createConnection = async () => {
+    connection = mysql.createConnection({
+        host: "remotemysql.com",
+        port: 3306,
+        user: "uKvABK0n58",
+        password: "g0FFzxiUpW",
+        database: "uKvABK0n58"
+    });
 
-const port = process.env.port || 3000;
+    connection.connect(err => {
+        if (err) {
+            console.log("Error Connecting to database");
+            console.log(err);
+            return;
+        } else {
+            console.log("Connected to database!");
+        }
+    });
 
-var randomGenerator = (done, length) => {
-  var flag = false;
-  var no;
-
-  while (!flag) {
-    no = Math.floor(Math.random() * length);
-
-    if (!done.has(no)) {
-      flag = true;
-    }
-  }
-  return no;
+    connection.on("error", function(err) {
+        console.log("Connection Loast");
+        createConnection();
+    });
 };
 
-connection.connect(err => {
-  if (err) {
-    console.log("Can't establish connection");
-  } else {
-    console.log("Connection established");
-  }
-});
+createConnection();
+
+var randomGenerator = (done, length) => {
+    var flag = false;
+    var no;
+
+    while (!flag) {
+        no = Math.floor(Math.random() * length);
+
+        if (!done.has(no)) {
+            flag = true;
+        }
+    }
+    return no;
+};
 
 processQuery = query => {
-  var promise = new Promise((resolve, reject) => {
-    connection.query(query, (error, results, fields) => {
-      if (error) {
-        console.log("Can't process '" + query + "'");
-        throw error;
-      }
+    var promise = new Promise((resolve, reject) => {
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                console.log("Can't process '" + query + "'");
+                throw error;
+            }
 
-      resolve(results);
+            resolve(results);
+        });
     });
-  });
 
-  return promise;
+    return promise;
 };
 
 app.get("/getword", async (req, res) => {
-  var word;
+    var word;
 
-  try {
-    results = await processQuery(
-      "SELECT WORD, ID FROM MEANING ORDER BY RAND() LIMIT 0,1 "
-    );
-    word = { name: results[0].WORD, id: results[0].ID };
-    res.json(word);
-  } catch (e) {
-    console.log("Promise rejected");
-  }
-});
-
-app.get("/abc/:id", async (req, res) => {
-  console.log(req.params.id);
+    try {
+        results = await processQuery(
+            "SELECT WORD, ID FROM MEANING ORDER BY RAND() LIMIT 0,1 "
+        );
+        word = { name: results[0].WORD, id: results[0].ID };
+        res.json(word);
+    } catch (e) {
+        console.log("Promise rejected");
+    }
 });
 
 app.get("/getdefs/:id", async (req, res) => {
-  var id = req.params.id;
-  var results = { defs: ["", "", ""], correct: -1 };
-  var done = new Set();
+    var id = req.params.id;
+    var results = { defs: ["", "", ""], correct: -1 };
+    var done = new Set();
 
-  try {
-    correct = await processQuery(
-      "select DEFINITION from MEANING where ID = " + id
-    );
+    try {
+        correct = await processQuery(
+            "select DEFINITION from MEANING where ID = " + id
+        );
 
-    no = randomGenerator(done, 3);
-    done.add(no);
-    console.log(no + 1);
+        no = randomGenerator(done, 3);
+        done.add(no);
+        console.log(no + 1);
 
-    results.defs[no] = correct[0].DEFINITION;
-    results.correct = no + 1;
+        results.defs[no] = correct[0].DEFINITION;
+        results.correct = no + 1;
 
-    options = await processQuery(
-      "SELECT DEFINITION FROM MEANING WHERE ID != " +
-        id +
-        " ORDER BY RAND() LIMIT 2"
-    );
+        options = await processQuery(
+            "SELECT DEFINITION FROM MEANING WHERE ID != " +
+                id +
+                " ORDER BY RAND() LIMIT 2"
+        );
 
-    for (let i = 0; i < 2; i++) {
-      no = randomGenerator(done, 3);
-      done.add(no);
-      results.defs[no] = options[i].DEFINITION;
+        for (let i = 0; i < 2; i++) {
+            no = randomGenerator(done, 3);
+            done.add(no);
+            results.defs[no] = options[i].DEFINITION;
+        }
+
+        res.json(results);
+    } catch (e) {
+        console.log("Promise rejected");
     }
-
-    res.json(results);
-  } catch (e) {
-    console.log("Promise rejected");
-  }
 });
 
 app.listen(port, () => {
-  console.log("Server has started on " + port);
+    console.log("Server has started on " + port);
 });
