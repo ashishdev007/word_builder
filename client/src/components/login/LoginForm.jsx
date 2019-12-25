c392ef17b224290feb5cc54c22510b11d88c9c70
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-25 10:29:43 -0600

    Login form ready

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -2,8 +2,79 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Login extends Component {
-    state = {};
+    state = {
+        username: null,
+        password: null,
+        errors: { username: false, password: false }
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        var { username, password } = this.state;
+        if (!username || !password) {
+            var errors = { username: false, password: false };
+            if (!username) {
+                errors.username = true;
+            }
+
+            if (!password) {
+                errors.password = true;
+            }
+
+            this.setState({ errors: errors });
+        } else {
+            console.log("Success");
+        }
+    };
+
+    handleInputChange = event => {
+        const target = event.target;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        var errors = this.state.errors;
+
+        if (value === "") {
+            errors[name] = true;
+            this.setState({ errors: errors });
+        } else {
+            errors[name] = false;
+            this.setState({
+                [name]: value,
+                errors: errors
+            });
+        }
+    };
+
+    errorClass = element => {
+        if (this.state.errors[element]) {
+            return "field error";
+        }
+
+        return "field";
+    };
+
+    buttonClass = () => {
+        var errors = this.state.errors;
+        if (errors.username || errors.password) {
+            return "ui primary large button disabled";
+        }
+        return "ui primary large button";
+    };
+
+    showWarning = element => {
+        if (this.state.errors[element]) {
+            return (
+                <div className="ui up pointing red basic label">
+                    Please enter a valid {element}!
+                </div>
+            );
+        }
+    };
+
     render() {
+        console.log(this.state);
         return (
             <div className="ui container" style={{ marginTop: "2vh" }}>
                 <div className="ui grid">
@@ -18,19 +89,41 @@ class Login extends Component {
                             </h1>
                             <div className="ui divider"></div>
 
-                            <div className="ui big form">
-                                <div className="field">
+                            <form
+                                className="ui big form"
+                                onSubmit={this.onSubmit}
+                            >
+                                <div className={this.errorClass("username")}>
                                     <label htmlFor="username">Username</label>
-                                    <input type="text" />
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        onFocus={() => {
+                                            this.setState({ utouch: true });
+                                        }}
+                                        onChange={this.handleInputChange}
+                                    />
+                                    {this.showWarning("username")}
                                 </div>
-                                <div className="field">
+                                <div className={this.errorClass("password")}>
                                     <label htmlFor="password">Password</label>
-                                    <input type="password" />
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onFocus={() => {
+                                            this.setState({ ptouch: true });
+                                        }}
+                                        onChange={this.handleInputChange}
+                                    />
+                                    {this.showWarning("password")}
                                 </div>
-                                <div className="ui primary large button">
+                                <button
+                                    className={this.buttonClass()}
+                                    type="submit"
+                                >
                                     Submit
-                                </div>
-                            </div>
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
